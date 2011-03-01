2ba513181dfa1df17b55e1b573ca0aefd9e2236f
zhanyinan <zhanyinan@sungomedical.com>
2011-03-01 13:49:25 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InductionCannon01::Application.config.secret_token = '4385d06ed9d614a54ad4e97fa77d87fb863b258a037a2f89a77272525a4eb01e0ec204dd27cc495f38af956bef88cfbb6e64a88607c0feef5e5f39b944824b21'
